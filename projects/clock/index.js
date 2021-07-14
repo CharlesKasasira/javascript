af450c6e94715f5b68db78d0c71549cff08843cd
CharlesKasasira <charleskasasira01@gmail.com>
2021-07-14 09:19:04 +0300

    started one project 2

--- projects/clock/index.js
+++ projects/clock/index.js
@@ -1,29 +1,15 @@
 const date = document.getElementById('date');
 const time = document.getElementById('hour');
 
-let d = new Date();
-var days = ['Sunday', 'Monday', 
-            'Tuesday', 'Wednesday', 
-            'Thursday', 'Friday', 
-            'Saturday', 'Sunday'];
-let s = d.getSeconds();
-let m = d.getMinutes();
-let h = d.getHours();
-setInterval(() => {
-    time.innerHTML = `${h}:${m}:${s}`;
-    s++;
-    if(s === 60){
-        s = 0;
-        m++;
-    }
-    if (m === 60){
-        m = 0;
-        h++;
-    }
-    if(h === 24){
-        h = 0;
-    }
-}, 1000);
+let watch = () => {
+    const DATE = new Date();
+    time.innerHTML = DATE.toLocaleTimeString();
+
+    let days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+    date.innerHTML = `${days[DATE.getDay()]} ${DATE.getDate()}/${DATE.getMonth()}/${DATE.getFullYear()}`;
+};
+const INTERVAL_ID = setInterval(watch, 1000);
 
 
-date.innerHTML = `${days[d.getDay()]} ${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+
